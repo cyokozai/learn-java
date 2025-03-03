61f20ef38c3147f686c4df28539ad040e5368258
cyokozai <yuu4suke27@outlook.jp>
2025-03-03 16:17:54 +0000

    Add additional Car instance and display its properties

--- classdeclaration/main.java
+++ classdeclaration/main.java
@@ -10,15 +10,26 @@ class Car {
 class toyota {
     public static void main(String[] args) {
         Car myCar = new Car();
-
+        Car yourCar = new Car();
+        
         myCar.car_number = 1234;
         myCar.gasoline   = 20.5;
         myCar.car_name   = "prius";
         myCar.car_color  = "Red";
 
+        yourCar.car_number = 5678;
+        yourCar.gasoline   = 30.5;
+        yourCar.car_name   = "corolla";
+        yourCar.car_color  = "Blue";
+
         System.out.println("Car Number: " + myCar.car_number);
         System.out.println("Gasoline: " + myCar.gasoline);
         System.out.println("Car Name: " + myCar.car_name);
         System.out.println("Car Color: " + myCar.car_color);
+
+        System.out.println("Car Number: " + yourCar.car_number);
+        System.out.println("Gasoline: " + yourCar.gasoline);
+        System.out.println("Car Name: " + yourCar.car_name);
+        System.out.println("Car Color: " + yourCar.car_color);
     }
 }
